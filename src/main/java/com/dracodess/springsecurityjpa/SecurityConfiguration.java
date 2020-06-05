bc283035f8c50a748162a3588cb945311a3e2290
mrkwapo <mrkwapo@gmail.com>
2020-06-05 01:40:10 -0400

    updated

--- src/main/java/com/dracodess/springsecurityjpa/SecurityConfiguration.java
+++ src/main/java/com/dracodess/springsecurityjpa/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //http.csrf().disable();
-        http.authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/").permitAll()
